079d1fb4c75638e8d5a6a44c689489d37a4998e0
Csinsin <120680325+Csinsin@users.noreply.github.com>
2023-03-04 23:17:57 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+TOKEN = "5611607894:AAE0WLWer36GNw5moYidohjCUsL3vkTwNB8"
+API_access_key = "XT8WyDT0kI4GFLmRszQiKx11sue4HTYZ"
+
+payload = {}
+headers= {
+  "apikey": API_access_key
+}
+
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+}
